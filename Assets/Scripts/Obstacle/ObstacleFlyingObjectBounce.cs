b5a21b0025862f708e4adeae8c3292c65b5b021f
yucheon6000 <yucheon6000@gmail.com>
2022-10-19 15:03:33 +0900

    날아오는 장애물(바운스) 추가

--- Assets/Scripts/Obstacle/ObstacleFlyingObjectBounce.cs
+++ Assets/Scripts/Obstacle/ObstacleFlyingObjectBounce.cs
@@ -4,4 +4,40 @@ using UnityEngine;
 
 public class ObstacleFlyingObjectBounce : ObstacleFlyingObject
 {
+    [Header("Bounce")]
+    [SerializeField]
+    private float bounceColliderDisableTimeAtFirst = 3;
+    [SerializeField]
+    private LayerMask targetBounceLayerMask;
+    [SerializeField]
+    private int targetBounceCount = 3;
+    private int currentBounceCount = 0;
+    [SerializeField]
+    private Collider bounceCollider;
+
+    private void Start()
+    {
+        bounceCollider.enabled = false;
+    }
+
+    protected override void AddForce()
+    {
+        base.AddForce();
+        Invoke(nameof(EnableBounceCollider), bounceColliderDisableTimeAtFirst);
+    }
+
+    private void EnableBounceCollider()
+    {
+        bounceCollider.enabled = true;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (((1 << other.gameObject.layer) & targetBounceLayerMask) == 0) return;
+
+        currentBounceCount++;
+
+        if (currentBounceCount >= targetBounceCount)
+            bounceCollider.enabled = false;
+    }
 }
