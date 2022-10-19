04cd057c56b40497bc397ec4f9c4f06a2ab24550
yucheon6000 <yucheon6000@gmail.com>
2022-10-19 14:22:22 +0900

    날아오는 장애물(일반) 추가

---
+++ Assets/Scripts/Obstacle/ObstacleFlyingObjectNormal.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleFlyingObjectNormal : ObstacleFlyingObject
+{
+    [SerializeField]
+    private Vector3 flyingForce;
+    [SerializeField]
+    private float knockbackForceScale = 1;
+
+    private new Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GameObjectUtils.FindCompoenet<Rigidbody>(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        rigidbody.isKinematic = true;
+        Invoke(nameof(AddForce), 1f);
+    }
+
+    private void AddForce()
+    {
+        rigidbody.isKinematic = false;
+        // rigidbody.velocity = Vector3.zero;
+        rigidbody.AddForce(flyingForce, ForceMode.Impulse);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (GameObjectUtils.FindCompoenet<PlayerMovement>(other.gameObject, out PlayerMovement movement))
+        {
+            movement.Knockback(rigidbody.velocity * knockbackForceScale);
+
+            if (GameObjectUtils.FindCompoenet<PlayerStatus>(other.gameObject, out PlayerStatus status))
+                status.IncreaseDustCount(-2);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + flyingForce);
+    }
+}
