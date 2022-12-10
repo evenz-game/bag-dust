c3eaa70a870f21860ead048019775c0cf600bd82
yucheon6000 <yucheon6000@gmail.com>
2022-12-10 20:17:25 +0900

    InputterSetting 수정
    - Reference InputterSetting을 추가할 수 있는 방법 추가

--- Assets/Scripts/Input/InputterSetting.cs
+++ Assets/Scripts/Input/InputterSetting.cs
@@ -6,6 +6,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "InputterSetting", menuName = "ScriptableObjects/InputterSetting", order = 1)]
 public class InputterSetting : ScriptableObject
 {
+    public InputterSetting reference;
+
+    [ContextMenu("Sync")]
+    private void Sync()
+    {
+        if (!reference) return;
+        this.xAxisName = reference.xAxisName;
+        this.yAxisName = reference.yAxisName;
+        this.axisScale = reference.axisScale;
+        this.buttonMappings = reference.buttonMappings;
+    }
+
+    [Space]
     public string xAxisName;
     public string yAxisName;
     public Vector2 axisScale = Vector2.one;
