57eeeb5d22d8106da7edc1f37a4ac105666ebaef
yucheon6000 <yucheon6000@gmail.com>
2022-12-13 03:35:43 +0900

    준비 애니메이션 적용

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -7,6 +7,8 @@ public class PlayerAnimator : PlayerComponent, PlayerModel.OnInitializedPlayerMo
     [SerializeField]
     private Animator animator;
 
+    private int modelIndex = 0;
+
     public void Dash()
     {
         animator?.Play("Dash", -1, 0);
@@ -17,8 +19,15 @@ public class PlayerAnimator : PlayerComponent, PlayerModel.OnInitializedPlayerMo
         animator?.Play("Win", -1, 0);
     }
 
+    public void Ready()
+    {
+        animator?.SetFloat("Model Index", modelIndex);
+        animator?.Play("Ready", -1, 0);
+    }
+
     public void OnInitializedPlayerModel(PlayerModelInfo model)
     {
         animator = model.Animator;
+        modelIndex = model.ModelIndex;
     }
 }
